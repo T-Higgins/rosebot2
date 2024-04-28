d493f6a9e050770882798170d277b27511299945
Tony H <53189296+T-Higgins@users.noreply.github.com>
2024-04-28 19:54:57 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 
 load_dotenv("config.env")
 
-BOT_TOKEN = environ.get("BOT_TOKEN", None)
+BOT_TOKEN = environ.get("7198587941:AAEQ2A3krDxrEtgf2hGuNgGYICd5sQoODUg", None)
 API_ID = int(environ.get("API_ID", 6))
 API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 API_ID1 = int(environ.get("API_ID1", 6))
